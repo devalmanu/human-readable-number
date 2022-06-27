c7ca24035b76abfcc6a76ca4f525f0364b91a0b2
Valiantsina Andreyeva <manuilova-1986@mail.ru>
2022-06-27 09:41:00 +0300

    solution task number to readable string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const first = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const thousand = ['', 'thousand'];
+
+    let wordNumber = '';
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    for (let i = 0; i < thousand.length; i++) {
+        let templateNumber = number % (100 * Math.pow(1000, i));
+
+        if (Math.floor(templateNumber / Math.pow(1000, i)) !== 0) {
+            if (Math.floor(templateNumber / Math.pow(1000, i)) < 20) {
+                wordNumber = (first[Math.floor(templateNumber / Math.pow(1000, i))] + thousand[i] + wordNumber).trim();
+            } else {
+                wordNumber = (tens[Math.floor(templateNumber / (10 * Math.pow(1000, i)))] + ' ' + first[Math.floor(templateNumber / Math.pow(1000, i)) % 10] + thousand[i] + wordNumber).trim();
+            }
+        }
+
+        templateNumber = number % (Math.pow(1000, i + 1));
+        if (Math.floor(templateNumber / (100 * Math.pow(1000, i))) !== 0) {
+            wordNumber = (first[Math.floor(templateNumber / (100 * Math.pow(1000, i)))] + ' hundred ' + wordNumber).trim();
+        }
+    }
+
+    return wordNumber;
+
 }
